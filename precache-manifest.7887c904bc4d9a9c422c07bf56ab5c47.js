5ab1f8cc9577c922cf88859d9073cc427756c6f0
Mona Lisa <vijayprakasht8@gmail.com>
2019-12-03 10:50:49 +0900

    Updates

---
+++ precache-manifest.7887c904bc4d9a9c422c07bf56ab5c47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fdddbb0bdc3fb66c8e195ad85e61f9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "938d05f981177e1049bc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cfa4ada1531444e206a6",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "938d05f981177e1049bc",
+    "url": "/robofriends/static/js/2.d73fc8a7.chunk.js"
+  },
+  {
+    "revision": "cfa4ada1531444e206a6",
+    "url": "/robofriends/static/js/main.bc00e0f8.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
